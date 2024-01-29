4e69ab15eee8263f7894ceaf570aa2d353a6cc93
Herland Flores Vargas <drack7585@gmail.com>
2024-01-29 18:39:44 -0400

    mi api seguridad

---
+++ WebApiEjemplo/Controllers/PersonaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiEjemplo.Entidades;
+
+namespace WebApiEjemplo.Controllers
+{
+    [ApiController]
+    [Route("api/persona")]
+    public class PersonaController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public PersonaController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(Persona persona)
+        {
+            context.Add(persona);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ WebApiEjemplo/Entidades/Persona.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiEjemplo.Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Ci { get; set; }
+    }
+}
